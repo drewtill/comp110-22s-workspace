6336a698445612a64b67d825f34d99d43b266678
Andrew Tilley <drewtill@email.unc.edu>
2022-01-14 13:57:39 -0500

    Finished Exercise 00! Also practice from LS03

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,5 @@
+"""My first program for COMP110."""
+
+__author__ = "730404552"
+
+print("Hello, world.")
---
+++ lessons/00_stored_program.py
@@ -0,0 +1,6 @@
+"""an example, simple program."""
+
+print("Hello World")
+quiz: int = 100
+print(quiz)
+print("Goodbye.")
